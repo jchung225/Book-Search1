5eaba8b2e540c9ddfe63f2432411df74997d49ee
Jim Chung <jchung225@gmail.com>
2020-01-05 19:01:35 -0500

    Updates

---
+++ precache-manifest.3fec8f8e9972a4efb98f06341c49630b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f785345a48232a19d65a",
+    "url": "/Book-Search1/static/css/main.7290c44f.chunk.css"
+  },
+  {
+    "revision": "f785345a48232a19d65a",
+    "url": "/Book-Search1/static/js/main.f785345a.chunk.js"
+  },
+  {
+    "revision": "0b942f6f529eac438873",
+    "url": "/Book-Search1/static/js/1.0b942f6f.chunk.js"
+  },
+  {
+    "revision": "effb29cd25cd95a9271a",
+    "url": "/Book-Search1/static/js/runtime~main.effb29cd.js"
+  },
+  {
+    "revision": "9348e128c514152683d5c22f07d3e7bb",
+    "url": "/Book-Search1/static/media/nyt-logo.9348e128.png"
+  },
+  {
+    "revision": "04602b7103fabb0adcfcbb3a546c3372",
+    "url": "/Book-Search1/index.html"
+  }
+];
